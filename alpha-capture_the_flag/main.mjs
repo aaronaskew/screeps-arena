99261d494f03da0e13ed54797ee5ef048fa2bc5f
Aaron Askew <aaronaskew@gmail.com>
2024-03-04 06:39:09 -0600

    fresh start with https://github.com/rustyscreeps/screeps-arena-starter-rust

--- alpha-capture_the_flag/main.mjs
+++
@@ -1,4 +0,0 @@
-import { initSync } from "./screeps-arena-starter-rust";
-import wasm_bytes from "./screeps-arena-starter-rust_bg.wasm.bin";
-initSync(wasm_bytes);
-export * from "./screeps-arena-starter-rust";
